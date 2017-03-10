a841f1703f8f11f37aa3146d40e53c89d79f7590
Chris <cgado12@gmail.com>
2017-03-10 18:58:21 -0500

    create db

---
+++ db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `locBucket`.`User` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `password` VARCHAR(105) NULL,
+  `role_id` INT NULL,
+  `birthday` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
