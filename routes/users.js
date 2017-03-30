2be2277f445c2b2baf99606a1c7166c077d14b8b
Kevin Dennis <kevindennis@mail.usf.edu>
2017-03-30 09:24:27 -0400

    Setup some structure for routes

--- routes/users.js
+++ routes/users.js
@@ -33,11 +33,13 @@ router.post('/register', function(req, res){
 
 	var errors = req.validationErrors();
 
+	//If there are errors, return to register page with errors
 	if(errors){
 		res.render('register',{
 			errors:errors
 		});
 	} else {
+		//Create the new user for the database
 		var newUser = new User({
 			name: name,
 			email:email,
@@ -45,6 +47,7 @@ router.post('/register', function(req, res){
 			password: password
 		});
 
+		//Store the new user
 		User.createUser(newUser, function(err, user){
 			if(err) throw err;
 			console.log(user);
@@ -56,14 +59,35 @@ router.post('/register', function(req, res){
 	}
 });
 
+router.post('/login',
+  passport.authenticate('local', {successRedirect:'/', failureRedirect:'/users/login',failureFlash: true}),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+router.get('/logout', function(req, res){
+	req.logout();
+
+	req.flash('success_msg', 'You are logged out');
+
+	res.redirect('/users/login');
+});
+
+//The below functions tell passport, the user manager, how to
+//	process the user logins
 passport.use(new LocalStrategy(
   function(username, password, done) {
+	 //To login using passport, each of the below function must not
+	 //	 interrupted by the false return done() statements
+
+	 //Check if the user exists
    User.getUserByUsername(username, function(err, user){
    	if(err) throw err;
    	if(!user){
    		return done(null, false, {message: 'Unknown User'});
    	}
 
+		//User exists, check that the password matches
    	User.comparePassword(password, user.password, function(err, isMatch){
    		if(err) throw err;
    		if(isMatch){
@@ -83,20 +107,6 @@ passport.deserializeUser(function(id, done) {
   User.getUserById(id, function(err, user) {
     done(err, user);
   });
-});
-
-router.post('/login',
-  passport.authenticate('local', {successRedirect:'/', failureRedirect:'/users/login',failureFlash: true}),
-  function(req, res) {
-    res.redirect('/');
-  });
-
-router.get('/logout', function(req, res){
-	req.logout();
-
-	req.flash('success_msg', 'You are logged out');
-
-	res.redirect('/users/login');
 });
 
 module.exports = router;
