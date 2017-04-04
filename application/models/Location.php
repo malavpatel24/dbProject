2c6d0519fa3f6ad888689b49c3ad0c5bdf16b2e6
Chris <cgado12@gmail.com>
2017-04-04 16:45:04 -0400

    wrote location functions

--- application/models/Location.php
+++ application/models/Location.php
@@ -16,7 +16,6 @@ class Location extends CI_Model {
      $rows = $query->result('Location');
 
      return $rows;
-
   }
 
   //Returns a location specified by id
@@ -42,26 +41,46 @@ class Location extends CI_Model {
   }
 
   //Returns the images for the location specified
-  public function get_location_images($id)
-  {
+  public function get_location_images($id) {
      $q_string = "SELECT l.name, l.description, p.pictures FROM locations l, pictures p
      WHERE id = ? AND p.location_id =?;";
      $query = $this->db->query($q_string,array($id));
      $rows = $query->result('Location');
 
      return $rows;
-
   }
 
-  //How do we want to implement this?
-  public function search_locations()
-  {
+  public function search_locations($name,$cost) {
+     if(cost == NULL){
+        search_by_name(name);
+     }else if( name == NULL){
+        search_by_cost(cost);
+     }else{
+        $q_string = "SELECT l.name,l.description,l.cost FROM locations l
+        WHERE name = ? AND cost <= ?";
+        $query = $this->db->query($q_string,array($name,$cost));
+        $rows - $query->result('Location');
+        return $rows;
+     }
+  }
 
+  public function search_by_name($name) {
+     $q_string = "SELECT l.name,l.description,l.cost FROM locations l
+     WHERE name = ?";
+     $query = $this->db->query($q_string,array($name));
+     $rows - $query->result('Location');
+     return $rows;
+  }
+  public function search_by_cost($cost) {
+     $q_string = "SELECT l.name,l.description,l.cost FROM locations l
+     WHERE cost <= ?";
+     $query = $this->db->query($q_string,array($cost));
+     $rows - $query->result('Location');
+     return $rows;
   }
 
   //Inserts a location into the database
-  public function create_location()
-  {
+  public function create_location() {
 
   }
 }
